1d98a5a5d585985a3acdbdb7c2a3b1e02d9b98d6
FehRenanPA <felipe.rennann@gmail.com>
2024-11-23 19:53:45 -0300

    Adicionando Procfile

--- Backend/app.py
+++ Backend/app.py
@@ -479,8 +479,8 @@ def new_home():
 
 
 if __name__ == '__main__':
-    #app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)
      # Inicia o servidor Flask
-    run_simple('127.0.0.1', 5000, app, use_reloader=True)
+    #run_simple('127.0.0.1', 5000, app, use_reloader=True)
     
       
