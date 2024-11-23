026db956640d6a51e289a4221337f6d51fbe859b
FehRenanPA <felipe.rennann@gmail.com>
2024-11-23 19:22:56 -0300

    Configuração do MongoDB no Heroku

--- Backend/migrar_dados_mongo.py
+++ Backend/migrar_dados_mongo.py
@@ -1,12 +1,18 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 import json
 
 # URI de conexão com MongoDB Atlas
-uri = "mongodb+srv://vr3_Engernharia:Construmaq1010@cluster0.3fjax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# Carregar as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+#uri = "mongodb+srv://vr3_Engernharia:Construmaq1010@cluster0.3fjax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri=os.getenv("MONGO_URI")
 client = MongoClient(uri)
 
 # Selecionar o banco de dados
-db = client['meu_banco']  # 'meu_banco' pode ser o nome do banco de dados que você deseja usar
+db = client['FUNCIONARIOS_VR3_PPAGAMENTOS']  # 'meu_banco' pode ser o nome do banco de dados que você deseja usar
 
 # Selecionar a coleção (tabela) onde os dados serão armazenados
 colecao = db['funcionarios']  # 'funcionarios' é o nome da coleção
