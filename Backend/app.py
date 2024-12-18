45d04789fe2874242691566cf0ac5d69698a361a
FehRenanPA <felipe.rennann@gmail.com>
2024-12-18 20:55:01 -0300

    Adicionando módulo gerar_sub_total_um1

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify, render_template, send_file, abort, Response
 from datetime import datetime
-from Backend.gerar_sub_total_um import Sub_total_um
-from gerador_olerite import Gerar_olerite
 from Backend.criar_cargo import CriarFuncionario
+from Backend.gerar_sub_total_um import Sub_total_um
+from Backend.gerador_olerite import Gerar_olerite
 import firebase_admin
 from firebase_admin import storage, credentials, firestore, auth
 from bson.objectid import ObjectId
